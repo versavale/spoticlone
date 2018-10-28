d961037e00fb2f70edf70de3fe0c8e752bd7fb95
Valentina Versari <versavale@gmail.com>
2018-10-28 17:52:06 +0100

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  
+  this.state = {
+    userInput: '', 
+/*     selectedBand: '', */
+    url: ''
+  }
+
+  this.handleChange = this.handleChange.bind(this);
+/*   this.handleSubmit = this.handleSubmit.bind(this); */
+  this.fetchData = this.fetchData.bind(this);
+
+  }
+/*   handleSubmit(){
+    const band = this.state.userInput.toString();
+    this.setState({
+      selectedBand: band
+    });
+  } */
+  handleChange(e){
+    this.setState({
+      userInput: e.target.value
+    });
+  }
+
+  fetchData() {
+    const band = this.state.userInput.toString();
+    const deezerUrl = 'https://api.deezer.com/' + band;
+    this.setState({
+      url: deezerUrl
+    });
+    /* fetch(deezerUrl, {method: 'GET', mode: 'no-cors'})
+    .then(res => res.json())
+    .then((out) => {
+        console.log('Output: ', out);
+}).catch(err => console.error(err)); */
+  }
+
   render() {
+    const displayBand = this.state.selectedBand;
+    const url = this.state.url;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="wrapper">
+          <input onChange = {this.handleChange}
+          value= {this.state.UserInput}
+          placeholder="your favorite artist"></input>
+          <button onClick={this.fetchData}>Search</button>
+        </div>
+        <p>{url}</p>
       </div>
     );
   }
