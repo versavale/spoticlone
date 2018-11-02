73fbeded591422733ae013daeef897a67a0aecb3
Valentina Versari <versavale@gmail.com>
2018-11-02 08:09:57 +0100

    Ãfunctioning json for imagesÃ

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ class App extends Component {
   
   this.state = {
     userInput: '',
-    url: ''
+    pic: ''
   }
 
   this.handleChange = this.handleChange.bind(this);
-  this.fetchURL = this.fetchURL.bind(this);
   this.fetchData = this.fetchData.bind(this);
 
   }
@@ -22,34 +21,31 @@ class App extends Component {
     });
   }
 
-  fetchURL() {
-    const band = this.state.userInput.toString();
-    const deezerUrl = 'https://api.deezer.com/search/artist/?q=' + band +'&index=0&limit=2&output=json';
-    this.setState({
-      url: deezerUrl
-    });
-  }
-
   fetchData() {
-    fetch(this.state.url, {method: 'GET', mode: 'no-cors'})
-    .then(res => res.json())
-    .then((out) => {
-
-        console.log('Output: ', out);
-}).catch(err => console.error(err));
+    const band = this.state.userInput.replace(/ /g,"-");
+    const fetch_url = 'https://api.deezer.com/search/artist/?q=' + band +'&index=0&limit=2&output=json';
+      fetch(fetch_url, {method: 'GET', mode: 'cors'})
+        .then(response => response.json())
+        .then(json => 
+          {this.setState({ 
+            pic: json.data[0].picture_big});
+      })
+      .catch(err => console.error(err));
   }
 
   render() {
-    const url = this.state.url;
+    const artistPicture = this.state.pic;
     return (
       <div className="App">
         <div className="wrapper">
           <input onChange = {this.handleChange}
           value= {this.state.UserInput}
           placeholder="your favorite artist"></input>
-          <button onClick={this.fetchURL}>Search</button>
+          <button onClick={this.fetchData}>Search</button>
         </div>
-        <a href={url}>{url}</a>
+        <div>
+        <img src={artistPicture} alt="Cheetah!" />
+        </div>
       </div>
     );
   }
