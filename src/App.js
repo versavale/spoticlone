b31489cd26af469d022334b366daa2879d797516
Valentina Versari <versavale@gmail.com>
2018-11-11 08:02:13 +0100

    added play pause svg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import play from './play.svg';
+import pause from './pause.svg';
 
 class App extends Component {
   constructor(props){
