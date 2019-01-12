0ca64ace0d9ba80489fce660647a5ee0ff9f494d
Valentina Versari <versavale@gmail.com>
2019-01-12 06:30:23 +0100

    bind removed

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ class App extends Component {
 
   this.handleChange = this.handleChange.bind(this);
   this.fetchData = this.fetchData.bind(this);
-  this.playMusic = this.playMusic.bind(this);
   }
 
   playMusic(arg) {
